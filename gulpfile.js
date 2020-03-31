72313495e2990173a78e8822d4bbbab34c88ab6f
Tumble <tumblegamer@gmail.com>
2020-03-31 11:29:20 +0100

    release

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,52 @@
-require('dotenv').config()
-var
-gulp = require('gulp'),
-uglify = require('gulp-uglify'),
-sass = require('gulp-sass'),
-concat = require('gulp-concat'),
-pipeline = require('readable-stream').pipeline,
-sourcemaps = require('gulp-sourcemaps'),
-release = require('gulp-github-release'),
-fs = require("fs"),
-verFile = 'version.txt',
-ver = 0;
+require("dotenv").config();
+var gulp = require("gulp"),
+  uglify = require("gulp-uglify"),
+  sass = require("gulp-sass"),
+  concat = require("gulp-concat"),
+  pipeline = require("readable-stream").pipeline,
+  sourcemaps = require("gulp-sourcemaps"),
+  release = require("gulp-github-release"),
+  fs = require("fs"),
+  verFile = "version.txt",
+  ver = 0;
 
+sass.compiler = require("node-sass");
 
-sass.compiler = require('node-sass');
-
-fs.readFile(verFile,function(err,buf){
-	if (err) console.log(err);
-	ver = parseInt(buf.toString());
-})
+fs.readFile(verFile, function(err, buf) {
+  if (err) console.log(err);
+  ver = parseInt(buf.toString());
+});
 
 function GetVersion() {
-	var v = ver++;
-	fs.writeFile(verFile, v, (err) => {
-		if (err) console.log(err);
-	  });
-	return v;
+  var v = ver++;
+  fs.writeFile(verFile, v, err => {
+    if (err) console.log(err);
+  });
+  return v;
 }
 
-
 function js(cb) {
-	return pipeline(
-		gulp.src('js/*.js'),
-		sourcemaps.init(),
-		uglify(),
-		concat('tumble.min.js'),
-		sourcemaps.write(),
-		gulp.dest('dist'),
-		cb
-	)
+  return pipeline(
+    gulp.src("js/*.js"),
+    sourcemaps.init(),
+    uglify(),
+    concat("tumble.min.js"),
+    sourcemaps.write(),
+    gulp.dest("dist"),
+    cb
+  );
 }
 
 function css(cb) {
-return pipeline(
-	gulp.src('scss/**/*.scss'),
-	sourcemaps.init(),
-	sass({outputStyle: 'compressed'}).on('error', sass.logError),
-	concat('tumble.min.css'),
-	sourcemaps.write(),
-	gulp.dest('dist'),
-	cb
-)
+  return pipeline(
+    gulp.src("scss/**/*.scss"),
+    sourcemaps.init(),
+    sass({ outputStyle: "compressed" }).on("error", sass.logError),
+    concat("tumble.min.css"),
+    sourcemaps.write(),
+    gulp.dest("dist"),
+    cb
+  );
 }
 
-function release(cb) {
-	var v = GetVersion();
-	return pipeline(
-		gulp.src('dist/*.*'),
-		release({
-			owner: 'tumble1999',
-			repo: 'tumble-css',
-			tag: v,
-		  }),
-		cb
-	)
-}
-
-exports.build = gulp.series(css,js,release);
+exports.build = gulp.series(css, js);
